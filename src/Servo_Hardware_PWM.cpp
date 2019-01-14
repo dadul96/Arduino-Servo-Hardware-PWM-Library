bd9ad7a9e32c6ef8c1dcc1afd2ced755ab383d48
Daniel Duller <46626392+dadul96@users.noreply.github.com>
2019-01-14 17:14:41 +0100

    Update Servo_Hardware_PWM.cpp

--- src/Servo_Hardware_PWM.cpp
+++ src/Servo_Hardware_PWM.cpp
@@ -1,49 +1,5 @@
 /*
-  Servo_Hardware_PWM.h - Library for using 6 Servos with fast, precise and lightweight hardware PWM.
-  Created by Daniel Duller, 11. January, 2019.
-  Changed by Daniel Duller, 13. January, 2019.
-
-  ###############################################################################
-  MIT License
-
-  Copyright (c) 2019 Daniel Duller
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-  ###############################################################################
-
-  
-  #########################################################
-  Only works on pin 2, 3, 7, 8, 44, and 45 on Arduino Mega!
-  #########################################################
-			Tested on Arduino Mega 2560 R3.
-  #########################################################
-
-  
-  The methods are:
-
-	Servo 				--- Class for manipulating servo motors connected to Arduino pins. (max. 6 elements)
-
-	attach(pin)  		--- Attaches a servo motor to an i/o pin. (only pin 2, 3, 7, 8, 44, and 45)
-
-	writeMicroseconds() --- Sets the servo pulse width in microseconds. (usable range between 500us and 2500us)
-
-	detach()    		--- Clears the used registers and stops the output of the attached pin.
+For information about the library, license and author, see Servo_Hardware_PWM.h - file.
 */
 
 #if defined(ARDUINO_ARCH_AVR)
