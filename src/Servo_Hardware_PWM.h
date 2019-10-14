b9a6bffa2636750bcee224e1844e54ca10b6551d
Daniel Duller <daniel.duller@gmx.net>
2019-10-14 15:44:27 +0200

    added a proper board support verification

--- src/Servo_Hardware_PWM.h
+++ src/Servo_Hardware_PWM.h
@@ -67,11 +67,13 @@
 
 #include <inttypes.h>
 
-#if !defined(ARDUINO_ARCH_AVR)
-#error "This library only supports boards with an AVR processor."
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+#warning "INFO: Servos can only be connected to the following pins: 2, 3, 7, 8, 44, and 45"
+#else
+#error "ERROR: This library only supports boards with an ATmega1280 or ATmega2560 processor. (Arduino/Genuino Mega/Mega1280/Mega2560)"
 #endif
 
-#define Servo_VERSION       1.0.1	//software version of this library
+#define Servo_VERSION       1.2.0	//software version of this library
 
 #define MIN_PULSE_WIDTH       500	//the shortest pulse sent to a servo  
 #define MAX_PULSE_WIDTH      2500	//the longest pulse sent to a servo 
